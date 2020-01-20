92b86bfe93ff29305062cabaa096e191189009d4
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-20 23:59:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance<@amount && @status == "pending"
+    if @sender.balance ,                               
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
