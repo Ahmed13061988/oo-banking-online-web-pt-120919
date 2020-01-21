53de0479ed15a6f6f3e28b6979a4a3cfe3f38fee
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-21 00:11:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,6 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
-      @sender.valid? == false 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
